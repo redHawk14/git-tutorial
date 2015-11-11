840dfe6d34ebd8707432017940d9158dc398e2b2
Arnoldo Avilés <arnoldo@gnovus.com>
2015-11-11 11:52:45 -0600

    Metodo para enviar mensaje a una cuenta

--- src/Mensajeria.java
+++ src/Mensajeria.java
@@ -2,6 +2,12 @@
 public class Mensajeria {
 
     public Mensajeria() {
+        // Constructor default
+    }
 
+
+    public void enviarMensaje(Cuenta destino, Mensaje m) {
+        // Envia el mensaje a la cuenta destino
+        destino.addMensaje(m);
     }
 }
